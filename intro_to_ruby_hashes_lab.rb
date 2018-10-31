1bb3cf6541ca64aa93d21831c0995068af823b20
Sean Langton <slangton1@gmail.com>
2018-10-31 03:54:14 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -32,7 +32,12 @@ def monopoly_with_third_tier
 		railroads: {
 			:pieces => 4,
 			:names => {},
-			:rent_in_dollars => {}
+			:rent_in_dollars => {
+				:one_piece_owned => 25,
+				:one_piece_owned => 50,
+				:one_piece_owned => 100,
+				:one_piece_owned => 200
+			}
 		}
 	}
 end
