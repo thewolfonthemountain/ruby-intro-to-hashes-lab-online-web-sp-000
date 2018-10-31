502cdb60ec71ee115ba726a482365a9478b12434
Sean Langton <slangton1@gmail.com>
2018-10-31 03:54:31 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -34,9 +34,9 @@ def monopoly_with_third_tier
 			:names => {},
 			:rent_in_dollars => {
 				:one_piece_owned => 25,
-				:one_piece_owned => 50,
-				:one_piece_owned => 100,
-				:one_piece_owned => 200
+				:two_piece_owned => 50,
+				:three_piece_owned => 100,
+				:four_piece_owned => 200
 			}
 		}
 	}
